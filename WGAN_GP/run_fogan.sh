4a0c3c6d9be28415a6d49cb28cdeafb8ec3f5a99
Calvin Seward <calvin.seward@zalando.de>
2018-02-09 21:06:40 +0100

    changed two data paths, they had my name in them

--- WGAN_GP/run_fogan.sh
+++ WGAN_GP/run_fogan.sh
@@ -1,6 +1,6 @@
 python3.5 -u main.py \
 --iterations 500000 \
---gan_divergence=WGANGP \
+--gan_divergence=FOGAN \
 --data_path='data/google_billion_word/1-billion-word-language-modeling-benchmark-r13output' \
 --is_train=True \
 --batch_size=64 \
@@ -10,7 +10,7 @@ python3.5 -u main.py \
 --learning_rate_d 0.0003 \
 --learning_rate_g 0.0001 \
 --load_checkpoint False \
---gradient_penalty=10. \
+--lipschitz_penalty=.1 \
 --seq_len=32 \
 --max_n_examples=10000000 \
 --n_ngrams=6 \
@@ -20,4 +20,6 @@ python3.5 -u main.py \
 --use_fast_lang_model \
 --num_sample_batches=100 \
 --jsd_test_interval=2000 \
+--gradient_penalty=1. \
+--activation_d=relu \
 --batch_norm_g \
